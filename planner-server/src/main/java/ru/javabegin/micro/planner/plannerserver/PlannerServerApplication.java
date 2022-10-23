55771e24fffbb01cec7262bd6651a2c5fa03cd36
Trebolll <M-or-ev@yandex.ru>
2022-10-23 16:32:12 +0700

    first commit and create module planner-server

---
+++ planner-server/src/main/java/ru/javabegin/micro/planner/plannerserver/PlannerServerApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.planner.plannerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerServerApplication.class, args);
+    }
+
+}
