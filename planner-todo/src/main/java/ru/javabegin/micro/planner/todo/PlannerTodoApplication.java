19cdc8975dff969e032c54c390c58a6b1425a674
Trebolll <M-or-ev@yandex.ru>
2022-10-24 20:16:59 +0700

    create PlannerTodoApplication

---
+++ planner-todo/src/main/java/ru/javabegin/micro/planner/todo/PlannerTodoApplication.java
@@ -0,0 +1,19 @@
+package ru.javabegin.micro.planner.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import ru.javabegin.backend.todo.entity.Category;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PlannerTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerTodoApplication.class, args);
+	}
+
+
+
+
+}
