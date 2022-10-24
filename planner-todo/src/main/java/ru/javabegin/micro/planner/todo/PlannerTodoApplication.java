ebf13281b494efaeb23a4dd935e0701460452e4a
Trebolll <M-or-ev@yandex.ru>
2022-10-24 22:23:24 +0700

    Finalization of the planner-todo module

--- planner-todo/src/main/java/ru/javabegin/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/ru/javabegin/micro/planner/todo/PlannerTodoApplication.java
@@ -3,10 +3,13 @@ package ru.javabegin.micro.planner.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import ru.javabegin.backend.todo.entity.Category;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages = {"ru.javabegin.micro.planner.plannerentity"})
+@EnableJpaRepositories(basePackages = {"ru.javabegin.micro.planner.plannerentity"})
 public class PlannerTodoApplication {
 
 	public static void main(String[] args) {
